921f83f0eaf87f11b0410e2fa892f91de75dee6a
hypnosmv <106038329+hypnosmv@users.noreply.github.com>
2022-08-02 00:42:30 +0200

    simple-3d-engine

--- src/Variables/Vector3f.java
+++ src/Variables/Vector3f.java
@@ -127,7 +127,7 @@ public class Vector3f {
         }
 
         // If only one point is outside, return a new polygon
-        // that adds one point to
+        // that contains an additional point
         if (outsidePointsCount == 1) {
             // Initialization
             Polygon newPolygon = new Polygon();
